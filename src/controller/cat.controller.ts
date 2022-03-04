fff391a6e79815113deb914ad16ca16b984fc0c7
Sangjun-man <molung@icloud.com>
2022-03-04 20:25:05 +0900

    init: git init

---
+++ src/controller/cat.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get } from '@nestjs/common';
+
+
+
+
+@Controller('cats')
+export class CatsController { 
+  @Get()
+    fildAll(): string { 
+      return "this action returns all cats";
+}
+}
+
+
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all cats';
+  }
+}
